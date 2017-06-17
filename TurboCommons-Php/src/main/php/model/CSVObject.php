5a6f9b6006a0790d76a041489199d0d25659c098
Jaume Mussons <jaumemussons79@gmail.com>
2017-06-17 19:34:33 +0200

    Php: Added CSVObject first version and tests

---
+++ TurboCommons-Php/src/main/php/model/CSVObject.php
@@ -0,0 +1,223 @@
+<?php
+
+/**
+ * TurboCommons is a general purpose and cross-language library that implements frequently used and generic software development tasks.
+ *
+ * Website : -> http://www.turbocommons.org
+ * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
+ * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
+ * CopyRight : -> Copyright 2015 Edertone Advanded Solutions (08211 Castellar del Vallès, Barcelona). http://www.edertone.com
+ */
+
+namespace org\turbocommons\src\main\php\model;
+
+use UnexpectedValueException;
+use org\turbocommons\src\main\php\utils\StringUtils;
+
+
+/**
+ * CSV data abstraction
+ */
+class CSVObject extends TableObject{
+
+
+    /**
+     * CSVObject stores all the information for a CSV document and provides easy access to all the
+     * columns and values and allows us to operate with it's data easily.
+     *
+     * @param string $string A string containing valid csv data
+     * @param boolean $headers Specifies if the first row of the provided csv data contains the column names or not. It is important to correctly set this value to avoid invalid data
+     * @param string $delimiter The character that is used as the csv delimiter. ',' is set by default
+     * @param string $enclosure The character that is used to escape fields when special characters are found
+     *
+     * @throws UnexpectedValueException
+     *
+     * @return CSVObject The constructed CSVObject
+     */
+    public function __construct($string = '', $headers = false, $delimiter = ',', $enclosure = '"'){
+
+        if(!StringUtils::isString($string)){
+
+            throw new UnexpectedValueException('CSVObject->constructor expected a string value');
+        }
+
+        parent::__construct();
+
+        if(StringUtils::isEmpty($string)){
+
+            return;
+        }
+
+        $currentRow = 0;
+        $currentColumn = 0;
+        $enclosureFound = false;
+        $fieldValue = '';
+        $stringLen = strlen($string);
+
+        for ($i = 0; $i < $stringLen; $i++) {
+
+            $character = substr($string, $i, 1);
+
+            if($character === $enclosure){
+
+                if($enclosureFound && substr($string, $i + 1, 1) === $enclosure){
+
+                    $fieldValue .= $character;
+                    $i++;
+
+                }else{
+
+                    $enclosureFound = !$enclosureFound;
+
+                    if($enclosureFound){
+
+                        $fieldValue = '';
+
+                    }else{
+
+                        $i = $this->_findNextDelimiterIndex($string, $i, $delimiter) - 1;
+                    }
+                }
+
+                continue;
+            }
+
+            if ($character === $delimiter && !$enclosureFound) {
+
+                $this->_insertField($currentRow, $currentColumn, $fieldValue);
+
+                $fieldValue = '';
+                $currentColumn ++;
+
+                continue;
+            }
+
+            if($character === "\r" || $character === "\n"){
+
+                if($enclosureFound){
+
+                    $fieldValue .= $character;
+
+                }else{
+
+                    if($currentColumn > 0){
+
+                        $this->_insertField($currentRow, $currentColumn, $fieldValue);
+
+                        $currentRow ++;
+                        $fieldValue = '';
+                        $currentColumn = 0;
+                    }
+                }
+
+                if($character === "\r" && substr($string, $i + 1, 1) === "\n"){
+
+                    if($enclosureFound){
+
+                        $fieldValue .= "\n";
+                    }
+
+                    $i++;
+                }
+
+                continue;
+            }
+
+            $fieldValue .= $character;
+        }
+
+        if($fieldValue != '' || $currentColumn >= $this->countColumns()){
+
+            $this->_insertField($currentRow, $currentColumn, $fieldValue);
+        }
+
+        if($headers){
+
+            $this->setColumnNames($this->getRow(0));
+            $this->removeRow(0);
+        }
+    }
+
+
+    /**
+     * Set the value for a csv cell
+     *
+     * @param integer $row An integer containing the index for the row that we want to set
+     * @param mixed $column An integer or a string containing the index or label for the column that we want to set
+     * @param string $value The value we want to set to the specified cell. Only string values are allowed
+     *
+     * @return mixed The assigned value after beign stored into the csv cell
+     */
+    public function setCell($row, $column, $value){
+
+        if(!StringUtils::isString($value)){
+
+            throw new UnexpectedValueException('CSVObject->setCell value must be a string');
+        }
+
+        return parent::setCell($row, $column, $value);
+    }
+
+
+    /**
+     * TODO
+     */
+    public function toString(){
+
+        // TODO
+    }
+
+
+    /**
+     * Auxiliary method that is used to add a new field to the table at the specified position
+     *
+     * @param integer $currentRow The row where we want to add the field
+     * @param integer $currentColumn The column where we want to add the field
+     * @param string $fieldValue The value we want to add to the field
+     *
+     * @return void
+     */
+    private function _insertField($currentRow, $currentColumn, $fieldValue){
+
+        if ($currentRow >= $this->countRows()){
+
+            $this->addRows(1);
+        }
+
+        if ($currentColumn >= $this->countColumns()){
+
+            $this->addColumns(1);
+        }
+
+        $this->setCell($currentRow, $currentColumn, $fieldValue);
+    }
+
+
+    /**
+     *Auxiliary method that looks for the next delimiter or newline characters on the csv string starting at the specified position.
+     *
+     * @param string &$string The full csv string to search in. This is passed by reference to avoid memory duplication for big csv files
+     * @param integer $currentIndex The csv string starting point for the search
+     * @param string $delimiter The character that is used as the csv delimiter
+     *
+     * @return integer The index where the next delimiter or newline character is found
+     */
+    private function _findNextDelimiterIndex(&$string, $currentIndex, $delimiter){
+
+        $stringLen = strlen($string);
+
+        for ($i = $currentIndex + 1; $i < $stringLen; $i++) {
+
+            $char = substr($string, $i, 1);
+
+            if($char === $delimiter || $char === "\r" || $char === "\n"){
+
+                return $i;
+            }
+        }
+
+        return $stringLen;
+    }
+}
+
+?>
---
+++ TurboCommons-Php/src/test/php/model/CSVObjectTest.php
@@ -0,0 +1,305 @@
+<?php
+
+/**
+ * TurboCommons is a general purpose and cross-language library that implements frequently used and generic software development tasks.
+ *
+ * Website : -> http://www.turbocommons.org
+ * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
+ * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
+ * CopyRight : -> Copyright 2015 Edertone Advanded Solutions (08211 Castellar del Vallès, Barcelona). http://www.edertone.com
+ */
+
+namespace org\turbocommons\src\test\php\model;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use stdClass;
+use org\turbocommons\src\main\php\managers\FilesManager;
+use org\turbocommons\src\main\php\model\CSVObject;
+use org\turbocommons\src\main\php\utils\CSVUtils;
+use org\turbocommons\src\main\php\utils\StringUtils;
+use org\turbocommons\src\main\php\model\JavaPropertiesObject;
+use org\turbocommons\src\main\php\utils\NumericUtils;
+
+
+/**
+ * CSVObjectTest
+ *
+ * @return void
+ */
+class CSVObjectTest extends PHPUnit_Framework_TestCase {
+
+
+    /**
+     * @see PHPUnit_Framework_TestCase::setUpBeforeClass()
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass(){
+
+        // Nothing necessary here
+    }
+
+
+    /**
+     * @see PHPUnit_Framework_TestCase::setUp()
+     *
+     * @return void
+     */
+    protected function setUp(){
+
+        $this->emptyValues = [null, [], new stdClass(), 0];
+        $this->emptyValuesCount = count($this->emptyValues);
+
+        $this->filesManager = new FilesManager();
+
+        $this->basePath = __DIR__.'/../resources/model/csvObject';
+
+        $this->csvFiles = [];
+        $files = $this->filesManager->getDirectoryList($this->basePath);
+
+        foreach ($files as $file) {
+
+            if(StringUtils::getFileExtension($file) === 'csv'){
+
+                $this->csvFiles[] = $file;
+            }
+        }
+    }
+
+
+    /**
+     * @see PHPUnit_Framework_TestCase::tearDown()
+     *
+     * @return void
+     */
+    protected function tearDown(){
+
+        // Nothing necessary here
+    }
+
+
+    /**
+     * @see PHPUnit_Framework_TestCase::tearDownAfterClass()
+     *
+     * @return void
+     */
+    public static function tearDownAfterClass(){
+
+        // Nothing necessary here
+    }
+
+
+	/**
+	 * testConstruct
+	 *
+	 * @return void
+	 */
+	public function testConstruct(){
+
+	    // Test empty values
+	    $exceptionMessage = '';
+
+        $test = new CSVObject();
+        $this->assertEquals(0, $test->countColumns());
+        $this->assertEquals(0, $test->countRows());
+
+        $test = new CSVObject('');
+        $this->assertEquals(0, $test->countColumns());
+        $this->assertEquals(0, $test->countRows());
+
+        $test = new CSVObject('     ');
+        $this->assertEquals(0, $test->countColumns());
+        $this->assertEquals(0, $test->countRows());
+
+        $test = new CSVObject("\n\n\n");
+        $this->assertEquals(0, $test->countColumns());
+        $this->assertEquals(0, $test->countRows());
+
+        for ($i = 0; $i < $this->emptyValuesCount; $i++) {
+
+            try {
+                new CSVObject($this->emptyValues[$i]);
+                $exceptionMessage = $this->emptyValues[$i].' empty value did not cause exception';
+            } catch (Exception $e) {
+                // We expect an exception to happen
+            }
+	    }
+
+	    // Test ok values
+
+	    // Simple one row empty csv
+	    $test = new CSVObject(',,');
+	    $this->assertEquals('', $test->getCell(0, 0));
+	    $this->assertEquals('', $test->getCell(0, 1));
+	    $this->assertEquals('', $test->getCell(0, 2));
+	    CSVUtils::isEqualTo(',,', $test->toString());
+
+	    // Simple one row empty csv with headers
+	    $test = new CSVObject("c1,c2,c3\r\n,,", true);
+	    $this->assertEquals('', $test->getCell(0, 'c1'));
+	    $this->assertEquals('', $test->getCell(0, 'c2'));
+	    $this->assertEquals('', $test->getCell(0, 'c3'));
+	    $this->assertEquals('', $test->getCell(0, 0));
+	    $this->assertEquals('', $test->getCell(0, 1));
+	    $this->assertEquals('', $test->getCell(0, 2));
+	    CSVUtils::isEqualTo("c1,c2,c3\n,,", $test->toString());
+
+	    // Simple one row csv without headers
+	    $test = new CSVObject('a,b,c');
+	    $this->assertEquals('a', $test->getCell(0, 0));
+	    $this->assertEquals('b', $test->getCell(0, 1));
+	    $this->assertEquals('c', $test->getCell(0, 2));
+	    CSVUtils::isEqualTo('a,b,c', $test->toString());
+
+	    // Simple one row csv with headers
+	    $test = new CSVObject("c1,c2,c3\n1,2,3", true);
+	    $this->assertEquals('1', $test->getCell(0, 'c1'));
+	    $this->assertEquals('2', $test->getCell(0, 'c2'));
+	    $this->assertEquals('3', $test->getCell(0, 'c3'));
+	    $this->assertEquals('1', $test->getCell(0, 0));
+	    $this->assertEquals('2', $test->getCell(0, 1));
+	    $this->assertEquals('3', $test->getCell(0, 2));
+	    CSVUtils::isEqualTo("c1,c2,c3\r\n1,2,3", $test->toString());
+
+	    // Simple one row csv without headers and scaped fields
+	    $test = new CSVObject('"a","b","c"');
+	    $this->assertEquals('a', $test->getCell(0, 0));
+	    $this->assertEquals('b', $test->getCell(0, 1));
+	    $this->assertEquals('c', $test->getCell(0, 2));
+	    CSVUtils::isEqualTo('a,b,c', $test->toString());
+
+	    // Simple one row csv with headers and scaped fields
+	    $test = new CSVObject("c1,c2,c3\r\"a\",\"b\",\"c\"", true);
+	    $this->assertEquals('a', $test->getCell(0, 'c1'));
+	    $this->assertEquals('b', $test->getCell(0, 'c2'));
+	    $this->assertEquals('c', $test->getCell(0, 'c3'));
+	    $this->assertEquals('a', $test->getCell(0, 0));
+	    $this->assertEquals('b', $test->getCell(0, 1));
+	    $this->assertEquals('c', $test->getCell(0, 2));
+	    CSVUtils::isEqualTo('c1,c2,c3\na,b,c', $test->toString());
+
+	    // Simple csv without headers and edge cases
+	    $test = new CSVObject(' a ,b  ,c  ');
+	    $this->assertEquals(' a ', $test->getCell(0, 0));
+	    $this->assertEquals('b  ', $test->getCell(0, 1));
+	    $this->assertEquals('c  ', $test->getCell(0, 2));
+	    CSVUtils::isEqualTo(' a ,b  ,c  ', $test->toString());
+
+	    // Multiple lines csv with different newline characters (windows: \r\n, Linux/Unix: \n, Mac: \r)
+	    $test = new CSVObject("1,2,3\na,b,c\r\n4,5,6\r");
+	    $this->assertEquals('1', $test->getCell(0, 0));
+	    $this->assertEquals('2', $test->getCell(0, 1));
+	    $this->assertEquals('3', $test->getCell(0, 2));
+	    $this->assertEquals('a', $test->getCell(1, 0));
+	    $this->assertEquals('b', $test->getCell(1, 1));
+	    $this->assertEquals('c', $test->getCell(1, 2));
+	    $this->assertEquals('4', $test->getCell(2, 0));
+	    $this->assertEquals('5', $test->getCell(2, 1));
+	    $this->assertEquals('6', $test->getCell(2, 2));
+	    CSVUtils::isEqualTo("1,2,3\na,b,c\r\r4,5,6\r\n", $test->toString());
+	    $this->assertTrue($test->countColumns() === 3);
+	    $this->assertTrue($test->countRows() === 3);
+
+	    // Simple csv without headers and scaped fields and characters with edge cases
+	    $test = new CSVObject(' """"" 1",",,,2",    "3", "4,"   ,  "5 " ');
+	    $this->assertEquals('"" 1', $test->getCell(0, 0));
+	    $this->assertEquals(',,,2', $test->getCell(0, 1));
+	    $this->assertEquals('3', $test->getCell(0, 2));
+	    $this->assertEquals('4,', $test->getCell(0, 3));
+	    $this->assertEquals('5 ', $test->getCell(0, 4));
+	    CSVUtils::isEqualTo('""""" 1",",,,2","3","4,","5 "', $test->toString());
+
+	    // Simple two row csv without headers and scaped fields and characters
+	    $test = new CSVObject("\"1\",\"2\",\"3\"\r\n\"a\"\"a\",\"b\",\"c\"");
+	    $this->assertEquals('1', $test->getCell(0, 0));
+	    $this->assertEquals('2', $test->getCell(0, 1));
+	    $this->assertEquals('3', $test->getCell(0, 2));
+	    $this->assertEquals('a"a', $test->getCell(1, 0));
+	    $this->assertEquals('b', $test->getCell(1, 1));
+	    $this->assertEquals('c', $test->getCell(1, 2));
+	    CSVUtils::isEqualTo("\"1\",\"2\",\"3\"\r\n\"a\"\"a\",\"b\",\"c\"", $test->toString());
+
+	    // Simple two row csv with headers and mixed scaped and non scaped fields and characters
+	    $test = new CSVObject("c1,\"c,\"\"2\",c3\r1,\"2\", 3 \r\n\"a \"\",a\",b,\"c\"", true);
+	    $this->assertEquals(['c1', 'c,"2', 'c3'], $test->getColumnNames());
+	    $this->assertEquals('1', $test->getCell(0, 'c1'));
+	    $this->assertEquals('2', $test->getCell(0, 'c,"2'));
+	    $this->assertEquals(' 3 ', $test->getCell(0, 'c3'));
+	    $this->assertEquals('1', $test->getCell(0, 0));
+	    $this->assertEquals('2', $test->getCell(0, 1));
+	    $this->assertEquals(' 3 ', $test->getCell(0, 2));
+	    $this->assertEquals('a ",a', $test->getCell(1, 'c1'));
+	    $this->assertEquals('b', $test->getCell(1, 'c,"2'));
+	    $this->assertEquals('c', $test->getCell(1, 'c3'));
+	    $this->assertEquals('a ",a', $test->getCell(1, 0));
+	    $this->assertEquals('b', $test->getCell(1, 1));
+	    $this->assertEquals('c', $test->getCell(1, 2));
+	    CSVUtils::isEqualTo("c1,\"c,\"\"2\",c3\r1,\"2\", 3 \r\n\"a \"\",a\",b,\"c\"", $test->toString());
+
+	    // test csv files from resources
+	    // Expected values are stored on a properties file for each one of the csv files.
+	    // It must have exactly the same name but with .properties extension
+	    foreach ($this->csvFiles as $file) {
+
+	        $fileData = $this->filesManager->readFile($this->basePath.'/'.$file);
+	        $test = new CSVObject($fileData, StringUtils::countStringOccurences($file, 'WithHeader') === 1);
+
+	        $resultFile = StringUtils::getFileNameWithoutExtension($file).'.properties';
+	        $resultData = new JavaPropertiesObject($this->filesManager->readFile($this->basePath.'/'.$resultFile));
+
+	        $this->assertEquals($resultData->get('rows'), $test->countRows(), 'File: '.$file);
+	        $this->assertEquals($resultData->get('cols'), $test->countColumns(), 'File: '.$file);
+
+	        foreach ($resultData->getKeys() as $key) {
+
+	            if($key !== 'rows' && $key !== 'cols'){
+
+	                $rowCol = explode('-', $key, 2);
+
+	                $columnFormatted = NumericUtils::isNumeric($rowCol[1]) ? (int)$rowCol[1] : $rowCol[1];
+
+	                $expected = $resultData->get($key);
+	                $value = $test->getCell((int)$rowCol[0], $columnFormatted);
+
+	                $this->assertEquals($expected, $value, 'File: '.$file);
+	            }
+	        }
+	    }
+
+	    // Test wrong values
+	    // TODO
+
+	    // Test exceptions
+	    // TODO
+
+	    if($exceptionMessage != ''){
+
+	        $this->fail($exceptionMessage);
+	    }
+	}
+
+
+	/**
+	 * testTodo
+	 *
+	 * @return void
+	 */
+	public function testTodo2(){
+
+	    // Test empty values
+	    // TODO
+
+	    // Test ok values
+	    // TODO
+
+	    // Test wrong values
+	    // TODO
+
+	    // Test exceptions
+	    // TODO
+	}
+
+}
+
+?>
