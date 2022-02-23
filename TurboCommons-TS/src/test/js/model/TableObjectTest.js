1bdc8383e9e4ceee0296607b5e1c4b3789841d0b
Jaume Mussons <jaumemussons79@gmail.com>
2022-02-23 11:47:41 +0100

    NA: Updated homepage url on all files

--- TurboCommons-TS/src/test/js/model/TableObjectTest.js
+++ TurboCommons-TS/src/test/js/model/TableObjectTest.js
@@ -3,7 +3,7 @@
 /**
  * TurboCommons is a general purpose and cross-language library that implements frequently used and generic software development tasks.
  *
- * Website : -> http://www.turbocommons.org
+ * Website : -> https://turboframework.org/en/libs/turbocommons
  * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
  * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
  * CopyRight : -> Copyright 2015 Edertone Advanded Solutions (08211 Castellar del Vallès, Barcelona). http://www.edertone.com
